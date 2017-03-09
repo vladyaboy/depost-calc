a47e5ab4472cd0b06394cddc2ecf1ff7776f7dee
Belov <ihatenetmail@gmail.com>
2017-03-09 10:38:15 +0700

    implement user input

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int main ()
+{
+
+	float vklad;
+	int srok;
+
+	printf ("Vvedite summu vklada");
+	scanf ("%f" , &vklad);
+
+	printf ("Vvedite srok vklada");
+	scanf ("%d" , &srok);
+
+	return 0;
+}
