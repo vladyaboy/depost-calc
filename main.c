2ee2ad3cc2f0c5da3f2333b5cc4b9c3be5888e8c
Belov <ihatenetmail@gmail.com>
2017-03-09 10:51:46 +0700

    user input validation

--- main.c
+++ main.c
@@ -5,12 +5,24 @@ int main ()
 
 	float vklad;
 	int srok;
+	
+	printf ("Vvedite summu vklada, summa doljna sostavlyat bolshe ili ravna 10000");
+        
+	while(1) 
+	{
+		scanf ("%f" , &vklad);
+        	if (vklad>=10000)break;
+        	else printf("Vasha summa ne korektna, vvedite zanogo\n);
+	}
 
-	printf ("Vvedite summu vklada");
-	scanf ("%f" , &vklad);
-
-	printf ("Vvedite srok vklada");
-	scanf ("%d" , &srok);
+	printf ("Vvedite srok vklada, srok doljen sostavlat ne bolee 365 dney");
+	
+	while(1)
+	{	
+		scanf ("%d" , &srok);
+		if(srok<=365)break;
+		else printf("Ne korektniy srok vklada, vvedite zanogo");
+	}
 
 	return 0;
 }
