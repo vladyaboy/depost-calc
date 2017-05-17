57d5f31f6565e1e9feaafa6cf3c8b065960704de
Betawind <ihatenetmail@gmail.com>
2017-05-17 23:44:42 +0700

    Fix main.c

--- test/main.c
+++ test/main.c
@@ -1,54 +1,12 @@
+#define CTEST_MAIN
+
 #include <ctest.h>
-#include <deposit.c>
 
-double v=99999;
-CTEST(caclulation_value_near, calculation_time30)
+int main(int argc, const char** argv)
 {
-	ASSERT_DBL_NEAR_TOL(99177, TaskCalculations(30, v), 1);
-}
-CTEST(caclulation_value_near, calculation_time31)
-{
-	ASSERT_DBL_NEAR_TOL(100168, TaskCalculations(31, v), 1);
-}
-CTEST(caclulation_value_near, calculation_time120)
-{
-	ASSERT_DBL_NEAR_TOL(100656, TaskCalculations(120, v), 1);
-}
-CTEST(caclulation_value_near, calculation_time121)
-{
-	ASSERT_DBL_NEAR_TOL(101988, TaskCalculations(121, v), 1);
-}
-CTEST(caclulation_value_near, calculation_time240)
-{
-	ASSERT_DBL_NEAR_TOL(103944, TaskCalculations(240, v), 1);
-}
-CTEST(caclulation_value_near, calculation_time241)
-{
-	ASSERT_DBL_NEAR_TOL(107922, TaskCalculations(241, v), 1);
+    return ctest_main(argc, argv);
 }
 
-double v1=100001;
-CTEST(caclulation_value_over, calculation_time30)
-{
-	ASSERT_DBL_NEAR_TOL(99179, TaskCalculations(30, v1), 1);
-}
-CTEST(caclulation_value_over, calculation_time31)
-{
-	ASSERT_DBL_NEAR_TOL(100255, TaskCalculations(31, v1), 1);
-}
-CTEST(caclulation_value_over, calculation_time120)
-{
-	ASSERT_DBL_NEAR_TOL(100987, TaskCalculations(120, v1), 1);
-}
-CTEST(caclulation_value_over, calculation_time121)
-{
-	ASSERT_DBL_NEAR_TOL(102653, TaskCalculations(121, v1), 1);
-}
-CTEST(caclulation_value_over, calculation_time240)
-{
-	ASSERT_DBL_NEAR_TOL(105261, TaskCalculations(240, v1), 1);
-}
-CTEST(caclulation_value_over, calculation_time241)
-{
-	ASSERT_DBL_NEAR_TOL(109905, TaskCalculations(241, v1), 1);
-}
+    Contact GitHub API Training Shop Blog About 
+
+
